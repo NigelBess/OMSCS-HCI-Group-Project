8a17eb059d42d64b13ea69a07af1c9d2c689ad30
unknown <nigelbess@gmail.com>
2025-04-15 01:50:46 -0700

    another attempt to deploy

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import {  Routes, Route, HashRouter } from 'react-router-dom'
 import Auth from './Auth.tsx'
 import Dashboard from './Dashboard.tsx'
 import { UserProvider } from './UserContext.tsx'
@@ -10,13 +10,13 @@ import { UserProvider } from './UserContext.tsx'
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <UserProvider>
-      <BrowserRouter basename={import.meta.env.BASE_URL}>
+    <HashRouter>
         <Routes>
           <Route path="/" element={<App />} />
           <Route path="/auth" element={<Auth />} />
           <Route path="/dashboard" element={<Dashboard />} />
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </UserProvider>
   </StrictMode>
   
